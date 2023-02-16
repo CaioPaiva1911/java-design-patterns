ada4fe8cb37ba132139aa96310ca1aad863b1786
Caio Paiva <caiowick@hotmail.com>
2023-02-15 22:20:33 -0300

    design patterns - Chain of Responsability

---
+++ src/br/com/alura/loja/descontos/CalculadoraDeDescontos.java
@@ -0,0 +1,16 @@
+package br.com.alura.loja.descontos;
+
+import br.com.alura.loja.orcamento.Orcamento;
+
+import java.math.BigDecimal;
+
+public class CalculadoraDeDescontos {
+    public BigDecimal calcular(Orcamento orcamento){
+        Desconto desconto = new DescontoParaOrcamentoMaisDeCincoItens(
+                new DescontoParaOrcamentoComValorMaiorQueQuinhentos(
+                        new SemDesconto())
+        );
+
+       return desconto.calcular(orcamento);
+    }
+}
