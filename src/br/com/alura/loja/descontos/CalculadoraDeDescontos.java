af4c4ec8d2869506fdeb2ecc46a8911a3b53957f
Caio Paiva <caiowick@hotmail.com>
2023-02-16 20:41:17 -0300

    design patterns - Template Method

--- src/br/com/alura/loja/descontos/CalculadoraDeDescontos.java
+++ src/br/com/alura/loja/descontos/CalculadoraDeDescontos.java
@@ -6,11 +6,11 @@ import java.math.BigDecimal;
 
 public class CalculadoraDeDescontos {
     public BigDecimal calcular(Orcamento orcamento){
-        Desconto desconto = new DescontoParaOrcamentoMaisDeCincoItens(
+        Desconto cadeiaDeDescontos = new DescontoParaOrcamentoMaisDeCincoItens(
                 new DescontoParaOrcamentoComValorMaiorQueQuinhentos(
                         new SemDesconto())
         );
 
-       return desconto.calcular(orcamento);
+       return cadeiaDeDescontos.calcular(orcamento);
     }
 }
